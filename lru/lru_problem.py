10cb7dd03bd69627439a85a1ef11c09df36dba38
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-04-14 18:13:47 +0530

    added empty methods for LRU

--- lru/lru_problem.py
+++ lru/lru_problem.py
@@ -1,4 +1,12 @@
 class lru():
-
     def __init__(self):
+        self.cache = []
+
+    def put(self, item):
+        pass
+
+    def get(self, item):
+        pass
+
+    def get_cache(self):
         pass
