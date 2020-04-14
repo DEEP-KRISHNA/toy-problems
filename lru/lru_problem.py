3b446f4345ad5fa56aece55550153b8734312e3e
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-04-14 18:50:34 +0530

    Main Logic for LRU was written, ALL Test cases were passed

--- lru/lru_problem.py
+++ lru/lru_problem.py
@@ -1,12 +1,44 @@
 class lru():
     def __init__(self):
-        self.cache = []
+        self.lrucache = []
 
     def put(self, item):
-        pass
+        if (len(self.lrucache) == 5):
+            if item in self.lrucache:
+                self.lrucache.remove(item)
+                self.lrucache.append(item)
+                return True
+            else:
+                self.lrucache.pop(0)
+                self.lrucache.append(item)
+                return True
+        else:
+            if item in self.lrucache:
+                self.lrucache.remove(item)
+                self.lrucache.append(item)
+                return True
+            else:
+                self.lrucache.append(item)
+                return True
 
     def get(self, item):
-        pass
+        if item in self.lrucache:
+            self.lrucache.remove(item)
+            self.lrucache.append(item)
+            return item
+        else:
+            return -1
 
     def get_cache(self):
-        pass
+        return self.lrucache
+
+
+def main():
+    l = lru()
+    l.put(1)
+    l.put(2)
+    l.put(3)
+
+
+if __name__ == '__main__':
+    main()
--- lru/lru_test.py
+++ lru/lru_test.py
@@ -7,13 +7,14 @@ def main():
     assert testobj.put(2) == True
     assert testobj.put(3) == True
     assert [1, 2, 3] == testobj.get_cache()
-    assert testobj.get(1) == True
+    assert testobj.get(1) == 1
     assert [2, 3, 1] == testobj.get_cache()
     assert testobj.put(4) == True
     assert testobj.put(5) == True
     assert [2, 3, 1, 4, 5] == testobj.get_cache()
     assert testobj.put(1) == True
     assert [2, 3, 4, 5, 1] == testobj.get_cache()
+    print("All Test Cases Passed")
 
 
 if __name__ == '__main__':
