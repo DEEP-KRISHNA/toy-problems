c1504dd0c417c156d9990c43e644662bb631ba19
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-04-14 18:39:51 +0530

    Test class was written and test cases were inserted

---
+++ lru/lru_test.py
@@ -0,0 +1,20 @@
+from lru_problem import lru
+
+
+def main():
+    testobj = lru()
+    assert testobj.put(1) == True
+    assert testobj.put(2) == True
+    assert testobj.put(3) == True
+    assert [1, 2, 3] == testobj.get_cache()
+    assert testobj.get(1) == True
+    assert [2, 3, 1] == testobj.get_cache()
+    assert testobj.put(4) == True
+    assert testobj.put(5) == True
+    assert [2, 3, 1, 4, 5] == testobj.get_cache()
+    assert testobj.put(1) == True
+    assert [2, 3, 4, 5, 1] == testobj.get_cache()
+
+
+if __name__ == '__main__':
+    main()
